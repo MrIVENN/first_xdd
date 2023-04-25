69b31b188b1a531a602dff2adc07d17836a50076
MrIVENN <grishan4ik07@gmail.com>
2023-04-25 21:27:40 +0300

    calculator

--- Iven's_project.py
+++ Iven's_project.py
@@ -1,7 +1,6 @@
-a = int(input())
-summ = 0
-while a != 0:
-    digital = a % 10
-    summ += digital
-    a //= 10
-print(summ)
+a = int(input('Add the number №1 - '))
+b = int(input('Add the number №2 - '))
+print(a, '+', b, '=', a+b)
+print(a, '-', b, '=', a-b)
+print(a, '*', b, '=', a*b)
+print(a, '/', b, '=', a//b)
